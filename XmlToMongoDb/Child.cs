1a60b2abf2ecd88e0726bb5d260ff3e2a269fa78
hamza <hamzakhan1937@hotmail.com>
2021-04-23 02:49:59 +0500

    first commit

---
+++ XmlToMongoDb/Child.cs
@@ -0,0 +1,92 @@
+﻿using System.Xml.Serialization;
+using System.Collections.Generic;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace XmlToMongoDb
+{
+    [XmlType("child")]
+    public class Child
+    {
+        [BsonElement("entire")]
+        [XmlElement("entire")]
+        public string Entire { get; set; }
+
+        [BsonElement("finest")]
+        [XmlElement("finest")]
+        public List<Finest> Finest { get; set; }
+
+        [BsonElement("rest")]
+        [XmlElement("rest")]
+        public int Rest { get; set; }
+
+        [BsonElement("low")]
+        [XmlElement("low")]
+        public string Low { get; set; }
+
+        [BsonElement("journey")]
+        [XmlElement("journey")]
+        public string Journey { get; set; }
+
+        [BsonElement("nearest")]
+        [XmlElement("nearest")]
+        public string Nearest { get; set; }
+    }
+
+    public class Finest
+    {
+        [BsonElement("obtain")]
+        [XmlElement("obtain")]
+        public Obtain Obtain { get; set; }
+
+        [BsonElement("teacher")]
+        [XmlElement("teacher")]
+        public string Teacher { get; set; }
+
+        [BsonElement("fifty")]
+        [XmlElement("fifty")]
+        public string Fifty { get; set; }
+
+        [BsonElement("want")]
+        [XmlElement("want")]
+        public int Want { get; set; }
+
+        [BsonElement("strong")]
+        [XmlElement("strong")]
+        public string Strong { get; set; }
+
+        [BsonElement("represent")]
+        [XmlElement("represent")]
+        public float Represent { get; set; }
+    }
+
+    public class Obtain
+    {
+        [BsonElement("put")]
+        [XmlElement("put")]
+        public string Put { get; set; }
+
+        [BsonElement("limit")]
+        [XmlAttribute("limit")]
+        public int Limit { get; set; }
+
+        [BsonElement("cowboy")]
+        [XmlElement("cowboy")]
+        public int Cowboy { get; set; }
+
+        [BsonElement("include")]
+        [XmlElement("include")]
+        public float Include { get; set; }
+
+        [BsonElement("sport")]
+        [XmlElement("sport")]
+        public string Sport { get; set; }
+
+        [BsonElement("grabbed")]
+        [XmlElement("grabbed")]
+        public int Grabbed { get; set; }
+
+        [BsonElement("ruler")]
+        [XmlElement("ruler")]
+        public string Ruler { get; set; }
+    }
+}
---
+++ XmlToMongoDb/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using MongoDB.Driver;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+
+namespace XmlToMongoDb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Child> result;
+            string fileName = "random.xml";
+
+            string currentDirectory = Environment.CurrentDirectory;
+            string projectDirectory = Directory.GetParent(currentDirectory).Parent.Parent.FullName;
+            string filePath = $"{projectDirectory}\\{fileName}";
+
+            // Ignore the root element in the xml.
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Child>), new XmlRootAttribute("root"));
+
+            Console.WriteLine($"Reading XML file with path: {filePath}");
+            try
+            {
+                if (!string.IsNullOrWhiteSpace(filePath))
+                {
+                    // Read file and deserialize the xml.
+                    using FileStream fileStream = new FileStream(filePath, FileMode.Open);
+                    result = (List<Child>)serializer.Deserialize(fileStream);
+
+                    if (result != null)
+                    {
+                        PopulateDB(result);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception occured while reading file: {ex}");
+            }
+        }
+
+        private static void PopulateDB(List<Child> values)
+        {
+            try
+            {
+                IMongoDatabase mongoDatabase;
+                IMongoCollection<Child> mongoCollection;
+
+                string database = "TestDB";
+                string collection = "Test";
+                string connectionString = "mongodb://localhost:27017";
+
+                // Creating the db connection.
+                var client = new MongoClient(connectionString);
+
+                if (client != null)
+                {
+                    mongoDatabase = client.GetDatabase(database);
+                    mongoCollection = mongoDatabase.GetCollection<Child>(collection);
+
+                    // Populating the db.
+                    mongoCollection.InsertMany(values);
+                    Console.WriteLine($"Data populated successfully in db: {database}");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception occured while populating db: {ex}");
+            }
+        }
+    }
+}
